bdb1a07a982af31765ecc20f393593f915fd9c7b
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-26 07:58:19 -0800

    Initial pug views for email

--- mail.js
+++ mail.js
@@ -24,7 +24,7 @@ exports.send = async (options) => {
   const text = htmlToText.fromString(html);
 
   const mailOptions = {
-    from: `Your Portfolio <noreply@LVSportfolio.com>`,
+    from: options.email,
     to: options.to,
     subject: options.subject,
     html,
