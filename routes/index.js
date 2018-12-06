5f23feabe35cdcfe98cdfb7bbcfe4b2505cf6b8a
Zach VanSomeren <zvansomeren@gmail.com>
2018-12-06 13:48:07 -0800

    Corrected async syntax

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,15 @@ const mail = require('../mail');
 
 const router = express.Router();
 
-router.post('/test', (req, res) => {
+router.post('/test', async (req, res) => {
   const { body } = req;
   body.to = 'lindsayvansomeren@gmail.com';
-  const response = mail.send({
+  const response = await mail.send({
     body,
     filename: 'portfolio-email',
     Subject: 'New Message from your portfolio',
   });
-  console.log(response);
-  // await res.send(response);
+  await res.send(response);
 });
 
 module.exports = router;
