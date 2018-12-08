af5eb141270e5a0c230d6bfdeb041eb60d5a042f
Zach VanSomeren <zvansomeren@gmail.com>
2018-12-08 09:32:22 -0800

    reset cors to default

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors({
-  origin: 'http://lindsayvansomeren.com'
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
